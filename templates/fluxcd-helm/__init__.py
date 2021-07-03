d71a86934afbc73401eba34d1cb426fd9ae9a27f
Sebastian Radloff <sradloff23@gmail.com>
2021-07-03 09:17:45 -0400

    Fixes release name.

--- templates/fluxcd-helm/__init__.py
+++ templates/fluxcd-helm/__init__.py
@@ -10,6 +10,7 @@ class HelmRelease(BaseObj):
         self.need("chart_version", "chart_version string needed")
         self.need("repository_name", "repository_name string needed")
         self.need("helm_values_files", "helm_values_files list needed")
+        self.need("component_version", "component_version list needed")
         self.update_root("templates/fluxcd-helm/HelmRelease.yml")
 
     def body(self):
@@ -19,6 +20,7 @@ class HelmRelease(BaseObj):
         self.root.spec.chart.spec.version = self.kwargs.chart_version
         self.root.spec.chart.spec.sourceRef.name = self.kwargs.repository_name
         self.root.spec.chart.spec.sourceRef.namespace = self.kwargs.namespace
+        self.root.spec.releaseName = self.kwargs.component_version
 
         root_values_dict = {}
         for file in self.kwargs.helm_values_files:
@@ -48,9 +50,10 @@ def main(input_params):
     repository_name = input_params.get("repository_name")
     repository_url = input_params.get("repository_url")
     helm_values_files = input_params.get("helm_values_files")
+    component_version = input_params.get("component_version")
 
     obj = BaseObj()
-    obj.root.HelmRelease = HelmRelease(name=name, namespace=namespace, chart_name=chart_name, chart_version=chart_version, repository_name=repository_name, helm_values_files=helm_values_files)
+    obj.root.HelmRelease = HelmRelease(name=name, namespace=namespace, chart_name=chart_name, chart_version=chart_version, repository_name=repository_name, helm_values_files=helm_values_files, component_version=component_version)
     obj.root.HelmRepository = HelmRepository(namespace=namespace, repository_name=repository_name, repository_url=repository_url)
 
     return obj
