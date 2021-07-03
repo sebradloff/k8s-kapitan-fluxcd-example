f54696ae745bb77dbb5fb515b5802fc6c2aeb87d
Sebastian Radloff <sradloff23@gmail.com>
2021-07-03 08:48:56 -0400

    Loads helm values file into HelmRelease resource.

--- templates/fluxcd-helm/__init__.py
+++ templates/fluxcd-helm/__init__.py
@@ -2,7 +2,7 @@ from kapitan.inputs.kadet import BaseObj, inventory
 
 inv = inventory()
 
-class HelmChart(BaseObj):
+class HelmRelease(BaseObj):
     def new(self):
         self.need("name", "name string needed")
         self.need("namespace", "namespace string needed")
@@ -10,15 +10,23 @@ class HelmChart(BaseObj):
         self.need("chart_version", "chart_version string needed")
         self.need("repository_name", "repository_name string needed")
         self.need("helm_values_files", "helm_values_files list needed")
-        self.update_root("templates/fluxcd-helm/HelmChart.yml")
+        self.update_root("templates/fluxcd-helm/HelmRelease.yml")
 
     def body(self):
         self.root.metadata.name = self.kwargs.name
         self.root.metadata.namespace = self.kwargs.namespace
-        self.root.spec.chart = self.kwargs.chart_name
-        self.root.spec.version = self.kwargs.chart_version
-        self.root.spec.sourceRef.name = self.kwargs.repository_name
-        self.root.spec.valuesFiles = self.kwargs.helm_values_files
+        self.root.spec.chart.spec.chart = self.kwargs.chart_name
+        self.root.spec.chart.spec.version = self.kwargs.chart_version
+        self.root.spec.chart.spec.sourceRef.name = self.kwargs.repository_name
+        self.root.spec.chart.spec.sourceRef.namespace = self.kwargs.namespace
+
+        root_values_dict = {}
+        for file in self.kwargs.helm_values_files:
+            values_obj = BaseObj()
+            values_obj.update_root(file)
+            root_values_dict.update(values_obj.to_dict())
+
+        self.root.spec["values"] = root_values_dict
 
 class HelmRepository(BaseObj):
     def new(self):
@@ -40,13 +48,9 @@ def main(input_params):
     repository_name = input_params.get("repository_name")
     repository_url = input_params.get("repository_url")
     helm_values_files = input_params.get("helm_values_files")
+
     obj = BaseObj()
-    obj.root.HelmChart = HelmChart(name=name, namespace=namespace, chart_name=chart_name, chart_version=chart_version, repository_name=repository_name, helm_values_files=helm_values_files)
+    obj.root.HelmChart = HelmRelease(name=name, namespace=namespace, chart_name=chart_name, chart_version=chart_version, repository_name=repository_name, helm_values_files=helm_values_files)
     obj.root.HelmRepository = HelmRepository(name=name, namespace=namespace, repository_name=repository_name, repository_url=repository_url)
 
     return obj
-    # return { "HelmChart": }
-    # return {
-    #     # "HelmRepository": HelmRepository(name=name, labels=labels, containers=[nginx_container]),
-    #     "HelmChart": HelmChart(name=name, namespace=namespace, chart_name=chart_name, chart_version=chart_version,repository_name=repository_name, helm_values_files=helm_values_files),
-    # }
